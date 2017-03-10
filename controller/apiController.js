df87081a603979e878b4a5ca3d37c26198916b62
shiyun <shiyun1013@163.com>
2017-03-10 18:10:05 +0800

    api delay 5000ms

--- controller/apiController.js
+++ controller/apiController.js
@@ -11,9 +11,18 @@ apiCtrl.registerUser = async (ctx, next) => {
 	console.log(`registerUser: `, ctx.request.body);
 	console.log(`registerUser: `, ctx.query);
 	console.log(`registerUser: `, ctx.params);
+
+	//模拟接口延迟
+	const f = () => {
+		return new Promise((resolve, reject) => {
+			setTimeout(()=>{resolve(333)}, 5000)
+		})
+	}
+	const a = await f();
 	ctx.body = {
 		username: '111乖乖',
 		password: '123456',
+		a
 	}
 }
 
--- routes/middlewares/respFormatter.js
+++ routes/middlewares/respFormatter.js
@@ -1,7 +1,21 @@
+const ApiError = require('./apiError');
+
 const urlFilter = (pattern) => {
 	return async (ctx, next) => {
 		let reg = new RegExp(pattern);
-		await next();
+		try{
+			await next();
+		}catch(error){
+			if(error instanceof ApiError && reg.test(ctx.originalUrl)){
+				ctx.status = 200;
+				ctx.body = {
+					code: error.code,
+					message: error.message
+				}
+			}
+
+			throw error;
+		}
 
 		if(reg.test(ctx.originalUrl)){
 			respFormatter(ctx);
