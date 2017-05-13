7a35a73dea6c907de2154b0913c195c8d5d3df49
shiy5 <yun.shi@dianrong.com>
2017-05-14 00:29:14 +0800

    rebuild demo.js

--- gulp/src/js/demo.js
+++ gulp/src/js/demo.js
@@ -1,6 +1,91 @@
 'use strict';
 import _ from 'lodash';
 
+const bMark = 'v-text';
+const initData = {date: new Date(), msg: 'xxx'};
+const root = document.getElementById('testa'),
+	  els = root.querySelectorAll(`[${bMark}]`);
+
+let bindData = {},
+	data = {};
+
+[].map.call(els, el => {
+	let vari = el.getAttribute(bMark);
+	bindData[vari] = {};
+});
+
+let bind = vari => {
+	bindData[vari].els = root.querySelectorAll(`[${bMark}="${vari}"]`);
+
+	[].map.call(bindData[vari].els, el => el.removeAttribute(bMark));
+
+	Object.defineProperty(data, vari, {
+		set: nval => {		
+			[].map.call(bindData[vari].els, el => {
+				if(el.textContent == nval) return;
+				bindData[vari].value = el.textContent = nval;		
+			});
+		},
+		get: _ => bindData[vari].value
+	});
+};
+
+_.map(bindData, (v, k) => bind(k));
+
+if(initData && _.isObject(initData)) {
+	_.map(initData, (v, k)=> {
+		data[k] = v;
+	});
+}
+
+
+const updateData = newData => {
+	_.map(newData, (v, k) => {
+		if(data.hasOwnProperty(k) && data[k] != v){
+			data[k] = v;
+		}
+	});
+};
+
+root.addEventListener('click', _ => {
+    updateData({
+        date: "2017-07-02",
+        msg: "world"
+    });
+},false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const bindingMark = 'v-text';
 
 let initData = {date: new Date(), msg: 'hello'}
@@ -8,7 +93,7 @@ let initData = {date: new Date(), msg: 'hello'}
 let bindings = {};
 let data = {};
 
-const root = document.getElementById('test'),
+const root = document.getElementById('testa'),
 	els = root.querySelectorAll(`[${bindingMark}]`);
 
 [].map.call(els, el => {
@@ -50,3 +135,4 @@ root.addEventListener('click', _ => {
         msg: "world"
     });
 },false);
+*/
